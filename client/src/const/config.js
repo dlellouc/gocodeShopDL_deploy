d5a56b346885b67b20b701d21fcaf926f7bda1e6
Deborah L <deborah.lellouche.dl@gmail.com>
2023-02-19 21:31:36 +0200

    changed: base url; changed : render-postbuild script

--- client/src/const/config.js
+++ client/src/const/config.js
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : 'someUrl.com'
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : 'https://supershop.onrender.com'
